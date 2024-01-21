f284949669bda5de1a647305293d890e7cc0213c
spykelionel <social.lionel@gmail.com>
2024-01-21 19:18:16 +0100

    the multiprocessing module

--- thread.py
+++ thread.py
@@ -22,3 +22,21 @@ t2.start()
 # Wait for both threads to finish
 t1.join()
 t2.join()
+
+from multiprocessing import Process
+
+def square_numbers(numbers):
+    for number in numbers:
+        print('Square:', number * number)
+
+if __name__ == '__main__':
+    numbers = [1, 2, 3, 4, 5]
+
+    # Create a process
+    process = Process(target=square_numbers, args=(numbers,))
+
+    # Start the process
+    process.start()
+
+    # Wait for the process to finish
+    process.join()
